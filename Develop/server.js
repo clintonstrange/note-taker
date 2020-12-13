d49ee406d362ca8fbec94e0a8d244da41f924e64
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2020-12-13 14:09:10 -0600

    server set up

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { notes } = require("./db/db");
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
