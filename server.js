0153dc88f538a14e406a8c3ad918d8031fe6a569
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2020-12-13 15:28:50 -0600

    set up get with param

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require("./db/db");
 
+function findByRouteName(routeName, notesArray) {
+  const result = notesArray.filter((note) => note.routeName === routeName)[0];
+  return result;
+}
+
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+app.get("/api/notes/:routeName", (req, res) => {
+  const result = findByRouteName(req.params.routeName, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(404).send("Note Not Found!");
+  }
 });
 
 app.listen(PORT, () => {
